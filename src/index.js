df8170e6c666996d7354340c8ad80706b6ab34ea
iDgoe <e.novykov@gmail.com>
2023-04-05 20:54:32 +0300

    dz1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+// fetch('https://restcountries.com/v3.1/name/canada');
+
+const countryIn = document.querySelector('#search-box');
+const countryOut = null;
+console.log(countryIn);
+console.log(countryOut);
+
+const inpunСountry = () => {
+  //   if (!nameIn.value) {
+  //     countryOut.textContent = 'Anonymous';
+  //   } else {
+  //   countryOut = countryIn.value;
+  console.log(countryIn.value);
+  fetch(`https://restcountries.com/v3.1/name/${countryIn.value}`);
+  //   }
+};
+
+countryIn.addEventListener(
+  'input',
+  debounce(() => {
+    inpunСountry();
+  }, DEBOUNCE_DELAY)
+);
